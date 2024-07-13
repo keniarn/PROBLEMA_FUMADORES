8b80eab4a92fe551c10d6a4d93872b98bd64cd85
Kes477 <kevines447@gmail.com>
2024-07-13 01:52:26 -0600

    Update Mesa.java

--- Solucion2-Kev/Mesa.java
+++ Solucion2-Kev/Mesa.java
@@ -1,10 +1,8 @@
 package Main;
 
 public class Mesa {
- // Estado de la mesa
- private volatile boolean estado=false; // Estado false = No hay recursos colocados
- public volatile int[] recursosColocados = new int[2]; // En la mesa se pueden colocar varios recursos, 
-                                               // todos los cuales son públicos
+ // Espacios de la mesa
+ public volatile int[] recursosEnLaMEsa = {-1,-1}; // -1 = espacio vacío
  
 	/**
 	 * @author Kevin Sánchez
@@ -12,8 +10,7 @@ public class Mesa {
 	 * Una vez que la mesa está llena con dos recursos, el estado pasa a true
 	 */
  public void colocarRecurso(int numRecurso, int IDrecurso) {
-	 this.recursosColocados[numRecurso]=IDrecurso;
-	 this.estado= (numRecurso==1)? true : false;
+	 this.recursosEnLaMEsa[numRecurso]=IDrecurso;
  }
  
 	/**
@@ -21,24 +18,7 @@ public class Mesa {
 	 * @summary Metodo ejecutado por un fumador para tomar un elemento de la mesa
 	 */
  public void quitarRecurso(int recursoAgarrado) {
-			 this.recursosColocados[recursoAgarrado]=-1;
-			 this.estado= (this.recursosColocados[0]+this.recursosColocados[1]==-2)? false : true; 
+			 this.recursosEnLaMEsa[recursoAgarrado]=-1;
 	 }
  
- 
- public void getMesa() {
-	 for(int i=0;i<2;i++)
-		 System.out.println(i + " tiene valor: " + recursosColocados[i]);
-	 if(this.estado)
-		 System.out.println("llena");
- }
- 
- // Set y get
- public void set_estado(boolean estado) {
- 	this.estado=estado;
- }
- 
- public boolean estaVacia() {
- 	return this.estado;
- }
 } // fin clase Mesa
